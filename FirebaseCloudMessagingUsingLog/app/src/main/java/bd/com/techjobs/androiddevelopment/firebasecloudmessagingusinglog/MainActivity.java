5330041312c1d82e26c5e460cc0bf18e07615dc6
Farhad2015 <mrforhad1@gmail.com>
2016-07-23 13:10:56 +0600

    First commit git push -u origin master clear exit

---
+++ FirebaseCloudMessagingUsingLog/app/src/main/java/bd/com/techjobs/androiddevelopment/firebasecloudmessagingusinglog/MainActivity.java
@@ -0,0 +1,13 @@
+package bd.com.techjobs.androiddevelopment.firebasecloudmessagingusinglog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
